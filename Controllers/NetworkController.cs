347b43e8f8ed42a7afd7f403386f5789f389f58b
Borys Lebeda <blebeda@luxoft.com>
2016-12-15 18:33:14 +0200

    Remove surplus recalculation

--- Controllers/NetworkController.cs
+++ Controllers/NetworkController.cs
@@ -124,11 +124,6 @@ namespace WebNetwork.Controllers
             if (null == valueSelector) { throw new ArgumentNullException("valueSelector"); }
             if (null == values) { throw new ArgumentNullException("values"); }
             ParameterExpression p = valueSelector.Parameters.Single();
-            // p => valueSelector(p) == values[0] || valueSelector(p) == ...
-            if (!values.Any())
-            {
-                return e => false;
-            }
             var equals = values.Select(value => (Expression)Expression.Equal(valueSelector.Body, Expression.Constant(value, typeof(TValue))));
             var body = equals.Aggregate<Expression>((accumulate, equal) => Expression.Or(accumulate, equal));
             return Expression.Lambda<Func<TElement, bool>>(body, p);
