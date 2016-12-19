55a4c36e010298ed82a240a7661a48a2d40980ec
borysl <borys.lebeda@gmail.com>
2016-12-19 08:54:21 +0200

    Get sigma renderer for canvas

--- wwwroot/_references.js
+++ wwwroot/_references.js
@@ -1,6 +1,7 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="js/homehomebrew.js" />
 /// <reference path="js/homesigma.js" />
+/// <reference path="js/sigma.renderers.canvas.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
